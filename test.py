0fc8827e9235c576d53ca3abfb5beaf0166f174b
sepidekoohfar <sepide1.koohfar@gmail.com>
2022-03-22 15:39:09 -0400

    share information across batches for both query and key

--- test.py
+++ test.py
@@ -101,7 +101,7 @@ def main():
                                           params['total_time_steps'] -
                                           params['num_encoder_steps']), 'r') as json_file:
         configs = json.load(json_file)
-        configs = configs["{}_{}".format(args.name, args.seed)]
+        configs = configs["{}_{}".format(args.nam)]
 
     batch_size = configs[0]
 
