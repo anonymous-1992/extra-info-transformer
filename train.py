cad751d6c36daf85c3d252cba1db7a57c663f412
sepidekoohfar <sepide1.koohfar@gmail.com>
2022-03-24 18:54:19 -0400

    watershed to univariate

--- train.py
+++ train.py
@@ -252,7 +252,7 @@ def evaluate():
 def main():
 
     study = optuna.create_study(direction="minimize", pruner=optuna.pruners.HyperbandPruner())
-    study.optimize(objective, n_trials=2, callbacks=[callback])
+    study.optimize(objective, n_trials=3, callbacks=[callback])
 
     pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
     complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
