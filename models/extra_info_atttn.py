a7820ca39c37e2cc09ec5ab177d7f03b9a58bc54
sepidekoohfar <sepide1.koohfar@gmail.com>
2022-03-30 20:52:09 -0400

    extra info attention only for enc dec

--- models/extra_info_atttn.py
+++ models/extra_info_atttn.py
@@ -85,7 +85,7 @@ class ScaledDotProductAttention(nn.Module):
             k_score = torch.einsum('bhkd,bhnd-> bhkn', K_prime, K) / np.sqrt(self.d_k)
             attn_k = self.softmax(k_score)
             K = torch.einsum('bhkn,bhnd->bhkd', attn_k, K)
-            scores = torch.zeros(2, b, h, Q.shape[2], l_k)
+            scores = torch.zeros(2, b, h, Q.shape[2], l_k).to(self.device)
             scores[0] = torch.einsum('bhqd,bhkd->bhqk', Q, K) / np.sqrt(self.d_k)
             scores[1] = torch.einsum('bhqd,bhkd->bhqk', Q, K_prime) / np.sqrt(self.d_k)
 
