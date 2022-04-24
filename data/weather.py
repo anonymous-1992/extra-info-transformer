60d4cd38ed3bf4532237e5a9fdcdc24adbb75184
sepidekoohfar <sepide1.koohfar@gmail.com>
2022-04-24 14:24:44 -0400

    train and test boundry in weather data

--- data/weather.py
+++ data/weather.py
@@ -38,7 +38,7 @@ class weatherFormatter(TrafficFormatter):
             ('hours_from_start', DataTypes.REAL_VALUED, InputTypes.KNOWN_INPUT),
         ]
 
-    def split_data(self, df, valid_boundary=4280, test_boundary=4628):
+    def split_data(self, df, valid_boundary=4280, test_boundary=4708):
         """Splits data frame into training-validation-test data frames.
         This also calibrates scaling object, and transforms data for each split.
         Args:
@@ -67,4 +67,4 @@ class weatherFormatter(TrafficFormatter):
         Returns:
           Tuple of (training samples, validation samples)
         """
-        return 102400, 8192
+        return 102400, 10240
