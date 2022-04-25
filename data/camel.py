81acb23d8eea1bf94069e73697d6d856c7c898e4
sepidekoohfar <sepide1.koohfar@gmail.com>
2022-04-25 14:20:27 -0400

    removing -999 and sort index of camel

--- data/camel.py
+++ data/camel.py
@@ -46,4 +46,4 @@ class camelFormatter(ElectricityFormatter):
         Returns:
           Tuple of (training samples, validation samples)
         """
-        return 512, 512
+        return 128000, 16384
