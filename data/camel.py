c634ccb8eefb9e86e6480d0e655ae3c14d8fa107
sepidekoohfar <sepide1.koohfar@gmail.com>
2022-04-25 12:32:35 -0400

    download and process camel dataset

---
+++ data/camel.py
@@ -0,0 +1,13 @@
+
+import sklearn.preprocessing
+from Utils import utils, base
+
+GenericDataFormatter = base.GenericDataFormatter
+DataTypes = base.DataTypes
+InputTypes = base.InputTypes
+
+
+class camelFormatter(GenericDataFormatter):
+    _column_definition = [
+
+    ]
