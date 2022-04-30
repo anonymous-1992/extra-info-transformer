dcff0a1d6a27e61fb0f83f805c8719052ef5b322
sepidekoohfar <sepide1.koohfar@gmail.com>
2022-04-30 17:37:40 -0400

    revert to max pool

--- train.py
+++ train.py
@@ -145,7 +145,7 @@ def objective(trial):
 
     d_model = trial.suggest_categorical("d_model", [32, 16])
     if "extra_info_attn" in args.attn_type:
-        n_ext_info = trial.suggest_categorical("n_ext_info", [log_b_size*4, log_b_size])
+        n_ext_info = trial.suggest_categorical("n_ext_info", [log_b_size*2, log_b_size])
         kernel_s = 1
         kernel_b = 1
     else:
