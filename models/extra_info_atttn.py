6b80b47e773a8d7b411901cc3d91e593bfaaa269
sepidekoohfar <sepide1.koohfar@gmail.com>
2022-04-30 09:43:00 -0400

    new attn for self and cross

--- models/extra_info_atttn.py
+++ models/extra_info_atttn.py
@@ -77,7 +77,7 @@ class ScaledDotProductAttention(nn.Module):
 
     def forward(self, Q, K, V, attn_mask):
 
-        if self.attn_type == "basic_attn" or not self.enc_attn:
+        if self.attn_type == "basic_attn":
 
             scores = torch.einsum('bhqd, bhkd -> bhqk', Q, K) / np.sqrt(self.d_k)
             if attn_mask is not None:
