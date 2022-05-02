a71aabda3535ca5f307d670e6ddc95a910db0ce2
sepidekoohfar <sepide1.koohfar@gmail.com>
2022-05-02 13:53:00 -0400

    add max pooling

--- train.py
+++ train.py
@@ -152,7 +152,7 @@ def objective(trial):
         n_ext_info = 0
         kernel_s = 1
         kernel_b = 1
-    if [d_model, n_ext_info, kernel_s, kernel_b] in param_history or n_distinct_trial > 5:
+    if [d_model, n_ext_info, kernel_s, kernel_b] in param_history or n_distinct_trial > 4:
         raise optuna.exceptions.TrialPruned()
     else:
         n_distinct_trial += 1
