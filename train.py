49afe58d1c9e67732f50d7fae602d969069e830d
sepidekoohfar <sepide1.koohfar@gmail.com>
2022-05-18 20:38:25 -0400

    dropout and d_model to 16

--- train.py
+++ train.py
@@ -145,7 +145,7 @@ def objective(trial):
     global val_loss
     global n_distinct_trial
 
-    d_model = trial.suggest_categorical("d_model", [32])
+    d_model = trial.suggest_categorical("d_model", [16])
     dr = trial.suggest_categorical("dr", [0, 0.1, 0.2])
     if "extra_info_attn" in args.attn_type:
         n_ext_info = 1
