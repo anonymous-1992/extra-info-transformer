d58263f5267df276f657905bdea518953a033260
sepidehkoohfar <sepide1.koohfar@gamil.com>
2022-06-16 20:43:46 -0400

    less historical data for testing

--- data/electricity.py
+++ data/electricity.py
@@ -220,8 +220,8 @@ class ElectricityFormatter(GenericDataFormatter):
         """Returns fixed model parameters for experiments."""
 
         fixed_params = {
-            'total_time_steps': 14 * 24,
-            'num_encoder_steps': 12 * 24,
+            'total_time_steps': 9 * 24,
+            'num_encoder_steps': 7 * 24,
             'num_epochs': 50,
         }
 
