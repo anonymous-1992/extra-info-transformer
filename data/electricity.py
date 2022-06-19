fee1ac7feb19483d73530644941d6e5c20f99617
sepidehkoohfar <sepide1.koohfar@gamil.com>
2022-06-19 15:22:38 -0400

    remove quassian for faster

--- data/electricity.py
+++ data/electricity.py
@@ -234,4 +234,4 @@ class ElectricityFormatter(GenericDataFormatter):
         Returns:
           Tuple of (training samples, validation samples)
         """
-        return 512, 512
+        return 128000, 15360
